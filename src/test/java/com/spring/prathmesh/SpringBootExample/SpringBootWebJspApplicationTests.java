a8b5486185e2efada46a9b7817ec7b24353b8ab2
prathmeshrajmane <prathmesh.rajmane1@gmail.com>
2021-09-15 19:35:43 +0530

    first commit

---
+++ src/test/java/com/spring/prathmesh/SpringBootExample/SpringBootWebJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.prathmesh.SpringBootExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
